8249bc04c1a9dc31767f49a9782559eb90f193a3
alinecbsr <alinecbsr@gmail.com>
2020-04-15 00:47:12 -0300

    Starting application

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './style.scss';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="container">
+      <h1>Starting application</h1>
+      </div>
+    )
+  }
+};
