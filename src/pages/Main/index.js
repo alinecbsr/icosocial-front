5af0cfc55aee5ba44108b53243f5d0a21bd1b514
alinecbsr <alinecbsr@gmail.com>
2020-05-06 22:45:40 -0300

    Global style standards

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import logo from '../../assets/image/logo2.svg'
+import React from 'react';
+import logo from '../../assets/image/logo2.svg';
 
-export default class Main extends Component {
-  render() {
-    return (
-      <div className="container">
-        <img src={logo} alt="Logo Icósocial"/>
-      </div>
-    )
-  }
-};
+function Main() {
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo Icósocial" />
+    </div>
+  );
+}
+
+export default Main;
