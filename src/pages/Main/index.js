b44889902c5207ee6d85381c842c7944b9263aee
alinecbsr <alinecbsr@gmail.com>
2020-05-10 22:42:04 -0300

    Implementation of 'Definitions' started

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../../components/Hero';
 import About from '../../components/About';
+import Definitions from '../../components/Definitions';
 
 function Main() {
   return (
@@ -8,6 +9,7 @@ function Main() {
       <Hero />;
       <div className="container">
         <About />
+        <Definitions />
       </div>
     </>
   );
