c2c0fb477005284523fbfc57719a14a77a364108
alinecbsr <alinecbsr@gmail.com>
2020-05-17 12:04:43 -0300

    'Our Story' component structure implementation

---
+++ src/components/OurStory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function OurStory() {
+  return (
+    <section className="container">
+      <div className="story">
+        <h2 className="story_title">Nossa História</h2>
+        <div className="story_column1">
+          <div className="story_column1-space">
+            <div className="story_column1-space--icon" />
+            <p />
+          </div>
+        </div>
+        <div className="story_column2">
+          <img src="" alt="" />
+        </div>
+        <div className="story_column3">
+          <div className="story_column3-space">
+            <div className="story_column3-space--icon" />
+            <p />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OurStory;
