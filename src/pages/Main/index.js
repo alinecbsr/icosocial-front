e3b1e166d8eb4754db06ef879c5d2d3074360433
Williams Gomes <williamsgomess@outlook.com>
2020-05-17 02:06:34 -0300

    Footer implementation
    Responsive component implementation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Hero from '../../components/Hero';
-import About from '../../components/About';
+// import About from '../../components/About';
+import Footer from '../../components/Footer';
 // import Definitions from '../../components/Definitions';
 
 function Main() {
   return (
     <>
       <Hero />
-      <About />
+      {/* <About /> */}
       {/* <Definitions /> */}
+      <Footer />
     </>
   );
 }
