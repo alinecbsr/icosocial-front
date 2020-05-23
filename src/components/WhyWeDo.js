a912544bd931b6c5310e4153be811249e024cd03
alinecbsr <alinecbsr@gmail.com>
2020-05-23 18:01:42 -0300

    Implemented 'WhyWeDo' component and its style

---
+++ src/components/WhyWeDo.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import figure from '../assets/image/imgWhy.svg';
+import data from '../assets/image/imgZone.svg';
+
+function WhyWeDo(props) {
+  return (
+    <section ref={props.rfs} className="container">
+      <h2>Por quem fazemos</h2>
+      <div className="who">
+        <article className="who_article">
+          <p className="who_article-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            lacinia, justo sit amet luctus pretium, purus ex facilisis lorem,
+            rutrum bibendum quam lectus viverra libero. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Donec in scelerisque felis. Nam
+            erat risus, dignissim a eros quis, lacinia sodales magna. Vivamus
+            accumsan risus in elit auctor condimentum a tempus lorem. Morbi sit
+            amet maximus urna. Etiam ac diam tincidunt, convallis neque et,
+            pharetra metus. Vivamus vulputate facilisis nulla et euismod.
+          </p>
+        </article>
+
+        <div className="weDo">
+          <div className="zone">
+            <img className="zone_img" src={data} alt="icon data" />
+            <p className="zone_text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              congue quis justo et ultricies. Etiam posuere nisl velit, eget
+              dictum felis porttitor ac. Aenean leo dui, consectetur ut cursus
+              non, blandit id leo. Aliquam tincidunt maximus risus quis
+              pellentesque
+            </p>
+            <p className="zone_number">100%</p>
+            <p className="zone_subtitle">Adesão</p>
+            <p className="zone_subtitle">escolar</p>
+          </div>
+
+          <div className="zone">
+            <img className="zone_img" src={data} alt="icon data" />
+            <p className="zone_text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              congue quis justo et ultricies. Etiam posuere nisl velit, eget
+              dictum felis porttitor ac. Aenean leo dui, consectetur ut cursus
+              non, blandit id leo. Aliquam tincidunt maximus risus quis
+              pellentesque
+            </p>
+            <p className="zone_number">95%</p>
+            <p className="zone_subtitle">Escolas</p>
+            <p className="zone_subtitle">públicas</p>
+          </div>
+
+          <div className="zone">
+            <img className="zone_img" src={data} alt="icon data" />
+            <p className="zone_text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              congue quis justo et ultricies. Etiam posuere nisl velit, eget
+              dictum felis porttitor ac. Aenean leo dui, consectetur ut cursus
+              non, blandit id leo. Aliquam tincidunt maximus risus quis
+              pellentesque
+            </p>
+            <p className="zone_number">85%</p>
+            <p className="zone_subtitle">Baixa</p>
+            <p className="zone_subtitle">renda</p>
+          </div>
+        </div>
+
+        <div className="why">
+          <p className="why_text">Não ser indiferente, faz toda a diferença!</p>
+          <img className="why_img" src={figure} alt="figure" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default WhyWeDo;
