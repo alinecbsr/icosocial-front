53e290810ab5fc11c62d443b3ebfe2603d4f3afc
alinecbsr <alinecbsr@gmail.com>
2020-06-01 21:22:42 -0300

    Implemented composes 'NavUser' and its style

---
+++ src/pages/UserHome/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import NavUser from '../../components/NavUser';
+
+export default function UserHome() {
+  return (
+    <>
+      <div className="main">
+        <NavUser />
+      </div>
+      <Footer />
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import Register from './pages/Register';
 // import RegisterConfirmation from './components/RegisterConfirmation';
 // import Login from './pages/Login';
-import ProfileSelection from './components/ProfileSelection';
+// import ProfileSelection from './components/ProfileSelection';
+import UserHome from './pages/UserHome';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         {/*  <Route path="/" exact component={Main} /> */}
-        <Route path="/" exact component={ProfileSelection} />
+        <Route path="/" exact component={UserHome} />
       </Switch>
     </BrowserRouter>
   );
